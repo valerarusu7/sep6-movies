d9d85915b91e2db654a388b0fdef4956772cae20
Valeriu Rusu <280167@via.dk>
2021-04-28 14:38:16 +0300

    Domain update#4

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://localhost:3000" : `https://${process.env.NOW_URL}`;
+export const server = dev ? "http://localhost:3000" : "https://sep6-movies.com";
