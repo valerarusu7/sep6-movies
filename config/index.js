38125333ded54325e375017a44b3d7d0644dbc0f
Valeriu Rusu <280167@via.dk>
2021-04-28 17:39:57 +0300

    Vercel url

--- config/index.js
+++ config/index.js
@@ -1,3 +1,5 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://localhost:3000" : process.env.VERCEL_URL;
+export const server = dev
+  ? "http://localhost:3000"
+  : process.env.NEXT_PUBLIC_VERCEL_URL;
