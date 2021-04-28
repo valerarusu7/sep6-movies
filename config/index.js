f93dcce230e20ab07b192a4301893f08b2044838
Valeriu Rusu <280167@via.dk>
2021-04-28 14:33:38 +0300

    Domain update#3

--- config/index.js
+++ config/index.js
@@ -1,3 +1,3 @@
 const dev = process.env.NODE_ENV !== "production";
 
-export const server = dev ? "http://localhost:3000" : process.env.VERCEL_URL;
+export const server = dev ? "http://localhost:3000" : `https://${process.env.NOW_URL}`;
