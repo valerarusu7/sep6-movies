cc2a65d412277bfa17641c54389b9c6cb4022360
Valeriu Rusu <280167@via.dk>
2021-04-28 18:02:24 +0300

    server side props v2

--- pages/index.js
+++ pages/index.js
@@ -20,13 +20,16 @@ function Home({
         <title>Home</title>
       </Head>
       <div>
-        <MoviesCategory movies={trending} categoryTitle={"Trending"} />
-        <MoviesCategory movies={topRated} categoryTitle={"Top Rated"} />
-        <MoviesCategory movies={netflix} categoryTitle={"Netflix Originals"} />
-        <MoviesCategory movies={action} categoryTitle={"Action"} />
-        <MoviesCategory movies={comedy} categoryTitle={"Comedy"} />
-        <MoviesCategory movies={horror} categoryTitle={"Horror"} />
-        <MoviesCategory movies={romance} categoryTitle={"Romance"} />
+        <MoviesCategory movies={trending || []} categoryTitle={"Trending"} />
+        <MoviesCategory movies={topRated || []} categoryTitle={"Top Rated"} />
+        <MoviesCategory
+          movies={netflix || []}
+          categoryTitle={"Netflix Originals"}
+        />
+        <MoviesCategory movies={action || []} categoryTitle={"Action"} />
+        <MoviesCategory movies={comedy || []} categoryTitle={"Comedy"} />
+        <MoviesCategory movies={horror || []} categoryTitle={"Horror"} />
+        <MoviesCategory movies={romance || []} categoryTitle={"Romance"} />
         <MoviesCategory
           movies={documentaries}
           categoryTitle={"Documentaries"}
@@ -37,43 +40,46 @@ function Home({
 }
 
 export async function getStaticProps() {
-  const netflixRequest = await axios.get(
-    `${server}${requests.server_requests.netflix}`
-  );
-  const trendingRequest = await axios.get(
-    `${server}${requests.server_requests.trending}`
-  );
-  const romanceRequest = await axios.get(
-    `${server}${requests.server_requests.romance}`
-  );
-  const actionRequest = await axios.get(
-    `${server}${requests.server_requests.action}`
-  );
-  const documentariesRequest = await axios.get(
-    `${server}${requests.server_requests.documentaries}`
-  );
-  const horrorRequest = await axios.get(
-    `${server}${requests.server_requests.horror}`
-  );
-  const topRatedRequest = await axios.get(
-    `${server}${requests.server_requests.topRated}`
-  );
-  const comedyRequest = await axios.get(
-    `${server}${requests.server_requests.comedy}`
-  );
-
-  return {
-    props: {
-      netflix: netflixRequest.data.netflix,
-      trending: trendingRequest.data.trending,
-      romance: romanceRequest.data.romance,
-      action: actionRequest.data.action,
-      documentaries: documentariesRequest.data.documentaries,
-      horror: horrorRequest.data.horror,
-      topRated: topRatedRequest.data.top_rated,
-      comedy: comedyRequest.data.comedy,
-    },
-  };
+  try {
+    const netflixRequest = await axios.get(
+      `${server}${requests.server_requests.netflix}`
+    );
+    const trendingRequest = await axios.get(
+      `${server}${requests.server_requests.trending}`
+    );
+    const romanceRequest = await axios.get(
+      `${server}${requests.server_requests.romance}`
+    );
+    const actionRequest = await axios.get(
+      `${server}${requests.server_requests.action}`
+    );
+    const documentariesRequest = await axios.get(
+      `${server}${requests.server_requests.documentaries}`
+    );
+    const horrorRequest = await axios.get(
+      `${server}${requests.server_requests.horror}`
+    );
+    const topRatedRequest = await axios.get(
+      `${server}${requests.server_requests.topRated}`
+    );
+    const comedyRequest = await axios.get(
+      `${server}${requests.server_requests.comedy}`
+    );
+    return {
+      props: {
+        netflix: netflixRequest.data.netflix,
+        trending: trendingRequest.data.trending,
+        romance: romanceRequest.data.romance,
+        action: actionRequest.data.action,
+        documentaries: documentariesRequest.data.documentaries,
+        horror: horrorRequest.data.horror,
+        topRated: topRatedRequest.data.top_rated,
+        comedy: comedyRequest.data.comedy,
+      },
+    };
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 export default Home;
