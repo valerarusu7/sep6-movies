c8d3e4d80f545ad34cbb18658569b40d275f0498
Valeriu Rusu <280167@via.dk>
2021-04-28 12:33:35 +0300

    Vercel cleanup

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,13 +1,11 @@
 import React from "react";
 import styles from "../styles/Navbar.module.css";
-import { useRouter } from "next/router";
 import { Avatar } from "@material-ui/core";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth } from "../firebase/firebase";
 
 const Navbar = () => {
   const [user] = useAuthState(auth);
-  const router = useRouter();
 
   const signOut = () => {
     auth.signOut();
@@ -15,10 +13,6 @@ const Navbar = () => {
 
   return user != null ? (
     <div className={styles.nav}>
-      <button onClick={() => router.push("/movie")}>movie</button>
-      <div>email: {user.email}</div>
-      <div>name: {user.displayName}</div>
-      <div>id: {user.uid}</div>
       <Avatar src={user.photoURL} alt={user.displayName} />
       <button onClick={() => signOut()}>Sign out</button>
     </div>
--- pages/movie.js
+++
@@ -1,9 +0,0 @@
-import React, { useEffect } from "react";
-
-const movie = ({}) => {
-  useEffect(() => {}, []);
-
-  return <div className="movie">Movie</div>;
-};
-
-export default movie;
